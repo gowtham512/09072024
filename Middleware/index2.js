6f18f6a4d35476c58237a6cbc0a762fe60ca66aa
Pateel Sai Gowtham Reddy <113625678+gowtham512@users.noreply.github.com>
2024-07-09 17:36:38 +0530

    Add files via upload

---
+++ Middleware/index2.js
@@ -0,0 +1,18 @@
+//Using morgan Middleware
+
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+const port = 3000;
+
+app.use(morgan("tiny"));
+
+app.get("/",(req,res)=>{
+  res.send("Hello")
+
+})
+
+app.listen(port,()=>{
+  console.log(`listening on port ${port}`)
+})
